8e3e87e3a319623d703cb37bbbf4a643143d6a8f
Ilya Anashkin <ii.anashkin@gmail.com>
2025-05-24 17:36:26 +0300

    bug: lint

--- calculate.py
+++ calculate.py
@@ -1,4 +1,3 @@
 def calculate_area(width, height):
-    if width < 0 or height < 0:
-        raise ValueError("Width and height must be non-negative")
+    if width < 0 or height < 0: raise ValueError("Width and height must be non-negative")
     return width * height
